6034735f3a67dde9f0b10cf6143cad45316fb926
deathy <deathy@deathy.ro>
2014-05-24 01:54:34 +0300

    * Add warmup runs, more normal runs and memory usage info

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+var util = require('util');
 var geoip = require("./geoip.js");
 
 function random_test_ip() {
@@ -15,30 +16,47 @@ function static_test_ip() {
 var ip_for_test = random_test_ip;
 
 function test() {
-
     var total = 0;
-    var numtests = 20;
+    var numtests = 100;
+    var numwarmups = 10;
     var numiterations = 1000000;
 
     console.log("starting test: geoip-native");
 
-    for (var t = 0; t < numtests; t++) {
+    function oneTest(warmup) {
 
-        var start = new Date().getTime();
+            var start = new Date().getTime();
 
-        for (var i = 0; i < numiterations; i++) {
-            geoip.lookup(ip_for_test());
-        }
+            for (var i = 0; i < numiterations; i++) {
+                geoip.lookup(ip_for_test());
+            }
 
-        var finish = new Date().getTime();
+            var finish = new Date().getTime();
 
-        total += (finish - start);
-        console.log("time for " + numiterations + " iterations: " + (finish - start) + " ms.");
-        console.log("time per lookup: " + ((finish - start) / numiterations) + " ms.");
+            if(!warmup) {
+                total += (finish - start);
+                console.log("time for " + numiterations + " iterations: " + (finish - start) + " ms.");
+                console.log("time per lookup: " + ((finish - start) / numiterations) + " ms.");
+            }
     }
+    // Warmup runs
+    for (var w = 0; w < numwarmups; w++) {
+        oneTest(true);
+    }
+    console.log("Memory usage after warmup:" );
+    console.log(util.inspect(process.memoryUsage()));
 
+    // Actual runs
+    for (var t = 0; t < numtests; t++) {
+        console.log("Starting test number "+t);
+        oneTest(false);
+    }
     console.log("average: " + (total / numtests));
-
+    console.log("Memory usage at end of test:" );
+    console.log(util.inspect(process.memoryUsage()));
 }
+
+console.log("Initial memory usage:" );
+console.log(util.inspect(process.memoryUsage()));
 
 setTimeout(test, 3000);
