c3a56eed9e0fb7aba4f250bf6eb03956c119a8f6
Andy Burke <aburke@bitflood.org>
2014-08-08 18:06:07 -0700

    Return city location data.

--- geoip.js
+++ geoip.js
@@ -1,64 +1,69 @@
-var autonomoussystems = [],
-    numautonomoussystems = 0;
+'use strict';
+
+var locations = [];
+var totalLocations = 0;
 
 var geoip = module.exports = {
 
     ready: false,
 
-    lookup: function (ip) {
+    lookup: function( ip ) {
 
-        if (!geoip.ready) {
-            return { error: "GeoIP not ready" };
+        if ( !geoip.ready ) {
+            return {
+                error: "GeoIP not ready"
+            };
         }
 
-        var ipl = iplong(ip);
+        var ipl = iplong( ip );
 
-        if (ipl == 0) {
-            return { error: "Invalid ip address " + ip + " -> " + ipl + " as integer" };
+        if ( ipl === 0 ) {
+            return {
+                error: "Invalid ip address " + ip + " -> " + ipl + " as integer"
+            };
         }
 
-        return find(ipl);
+        return find( ipl );
     }
 };
 
-function iplong(ip) {
+function iplong( ip ) {
 
-    if (!ip) {
+    if ( !ip ) {
         return 0;
     }
 
     ip = ip.toString();
 
-    if (isNaN(ip) && ip.indexOf(".") == -1) {
+    if ( isNaN( ip ) && ip.indexOf( "." ) == -1 ) {
         return 0;
     }
 
-    if (ip.indexOf(".") == -1) {
+    if ( ip.indexOf( "." ) == -1 ) {
 
         try {
-            ip = parseFloat(ip);
+            ip = parseFloat( ip );
             return ip < 0 || ip > 4294967296 ? 0 : ip;
         }
-        catch (s) {
-        }
+        catch ( s ) {}
     }
 
-    var parts = ip.split(".");
+    var parts = ip.split( "." );
 
-    if (parts.length != 4) {
+    if ( parts.length != 4 ) {
         return 0;
     }
 
     var ipl = 0;
 
-    for (var i = 0; i < 4; i++) {
-        parts[i] = parseInt(parts[i], 10);
+    for ( var i = 0; i < 4; i++ ) {
+        parts[ i ] = parseInt( parts[ i ], 10 );
 
-        if (parts[i] < 0 || parts[i] > 255) {
+        if ( parts[ i ] < 0 || parts[ i ] > 255 ) {
             return 0;
         }
 
-        ipl += parts[3 - i] * (Math.pow(256, i));
+        ipl += parts[ 3 - i ] * ( Math.pow( 256, i ) );
     }
 
     return ipl > 4294967296 ? 0 : ipl;
@@ -69,27 +74,25 @@ function iplong(ip) {
  * @param ip the ip we're looking for
  * @return {*}
  */
-function find(ipl) {
+function find( ipl ) {
 
-    var imax = numautonomoussystems-1, imin = 0;
+    var imax = totalLocations - 1,
+        imin = 0;
 
-    while (imax >= imin) {
-        // calculate the midpoint for roughly equal partition
-        var imid = Math.floor((imin + imax) / 2);
-        var autonomoussystem = autonomoussystems[imid];
-        if (autonomoussystem.ipstart <= ipl && autonomoussystem.ipend >= ipl) {
-            // key found at index imid
-            return autonomoussystem;
-            // determine which subarray to search
-        } else if (autonomoussystem.ipstart < ipl) {
-            // change min index to search upper subarray
+    while ( imax >= imin ) {
+        var imid = Math.floor( ( imin + imax ) / 2 );
+        var location = locations[ imid ];
+        if ( location.ipstart <= ipl && location.ipend >= ipl ) {
+            return location;
+        }
+        else if ( location.ipstart < ipl ) {
             imin = imid + 1;
-        } else {
-            // change max index to search lower subarray
+        }
+        else {
             imax = imid - 1;
         }
     }
-    // key was not found
+
     return undefined;
 }
 
@@ -98,32 +101,94 @@ function find(ipl) {
  * from MaxMind, you should be able to update it at any time by just
  * overwriting GeoIPASNum2.csv with a new version.
  */
-(function () {
+( function() {
 
-    var fs = require("fs");
-    var sys = require("sys");
-    var stream = fs.createReadStream(__dirname + "/GeoIPASNum2.csv");
-    var buffer = "";
+    var async = require( 'async' );
+    var extend = require( 'node.extend' );
+    var csv = require( 'csv-stream' );
+    var fs = require( 'fs' );
 
-    stream.addListener("data", function (data) {
-        buffer += data.toString().replace(/"/g, "");
-    });
+    var blocks = {};
+    
+    async.series( [
+        // load blocks
+        function( next ) {
 
-    stream.addListener("end", function () {
+            var csvStream = csv.createStream( {
+                columns: [ 'startIpNum', 'endIpNum', 'locId' ],
+                enclosedChar: '"'
+            } );
 
-        var entries = buffer.split("\n");
+            fs.createReadStream( __dirname + '/GeoLiteCity-Blocks.csv' ).pipe( csvStream )
+            .on( 'error', next )
+            .on( 'end', next )
+            .on( 'data', function( data ) {
+                var locId = parseInt( data.locId );
+                var start = parseInt( data.startIpNum );
+                var end = parseInt( data.endIpNum );
 
-        for (var i = 0; i < entries.length; i++) {
-            var entry = entries[i].split(",");
-            autonomoussystems.push({ipstart: parseInt(entry[0]), ipend: parseInt(entry[1]), asname: entry.slice(2).join(",")});
+                if ( isNaN( locId ) || isNaN( start ) || isNaN( end ) ) {
+                    return;
+                }
+
+                blocks[ locId ] = {
+                    start: start,
+                    end: end
+                };
+            } );
+        },
+
+        // load city data
+        function( next ) {
+            var csvStream = csv.createStream( {
+                columns: [ 'locId', 'country', 'region', 'city', 'postalCode', 'latitude', 'longitude', 'metroCode', 'areaCode' ],
+                enclosedChar: '"'
+            } );
+
+            fs.createReadStream( __dirname + '/GeoLiteCity-Location.csv' ).pipe( csvStream )
+            .on( 'error', next )
+            .on( 'end', next )
+            .on( 'data', function( data ) {
+                
+                var locId = parseInt( data.locId );
+                if ( isNaN( locId ) ) {
+                    return;
+                }
+
+                var block = blocks[ locId ];
+                if ( !block ) {
+                    return;
+                }
+                
+                var location = extend( data, {
+                    ipstart: blocks[ locId ].start,
+                    ipend: blocks[ locId ].end   
+                } );
+                
+                for ( var key in location ) {
+                    location[ key ] = location[ key ] || null;
+                }
+                
+                location.locId = parseInt( locId );
+                location.metroCode = location.metroCode ? parseInt( location.metroCode ) : location.metroCode;
+                location.areaCode = location.areaCode ? parseInt( location.areaCode ) : location.areaCode;
+                location.longitude = location.longitude ? parseFloat( location.longitude ) : location.longitude;
+                location.latitude = location.latitude ? parseFloat( location.latitude ) : location.latitude;
+
+                locations.push( location );
+            } );
         }
-
-        autonomoussystems.sort(function (a, b) {
-            return a.ipstart - b.ipstart;
+    ], function( error ) {
+        if ( error ) {
+            throw new Error( error );
+        }
+        
+        locations.sort( function( l, r ) {
+            return l.ipstart - r.ipstart;
         });
 
-        numautonomoussystems = autonomoussystems.length;
+        totalLocations = locations.length;
         geoip.ready = true;
-    });
+    } );
 
-}());
+}() );
